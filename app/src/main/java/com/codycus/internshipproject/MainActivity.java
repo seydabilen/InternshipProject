37e90dac1ac17009015548420fd752c182f150dc
seydabilen <seydabilen0@gmail.com>
2018-03-05 12:27:17 +0300

    Initial commit

---
+++ app/src/main/java/com/codycus/internshipproject/MainActivity.java
@@ -0,0 +1,21 @@
+package com.codycus.internshipproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int count=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view){
+        count++;
+        TextView text=(TextView)findViewById(R.id.textView);
+        text.setText(" "+count);
+
+    }
+}
