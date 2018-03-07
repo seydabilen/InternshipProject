1d3789ee2715316ef2a9510e854ac8bdc0042486
seydabilen <seydabilen0@gmail.com>
2018-03-07 10:03:34 +0300

    Buttons are programmatically created

--- app/src/main/java/com/codycus/internshipproject/MainActivity.java
+++ app/src/main/java/com/codycus/internshipproject/MainActivity.java
@@ -11,76 +11,124 @@ import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn,btn1;
     private ViewGroup rootLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rootLayout = (ViewGroup) findViewById(R.id.view_root);
-        btn1 = (Button) rootLayout.findViewById(R.id.button1);
-        btn = (Button)rootLayout.findViewById(R.id.button);
-
-        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(250, 250);
-        layoutParams.topMargin=300;
-        FrameLayout.LayoutParams layoutParams1 = new FrameLayout.LayoutParams(250, 250);
-        btn1.setLayoutParams(layoutParams);
-        btn.setLayoutParams(layoutParams1);
-        btn1.setOnTouchListener(new ChoiceTouchListener());
-        btn.setOnTouchListener(new ChoiceTouchListener());
-
-    }
-
-    private final class ChoiceTouchListener implements View.OnTouchListener {
-        private int _xdelta;
-        private int _ydelta;
 
 
-        @Override
-        public boolean onTouch(View view, MotionEvent event) {
+        for (int i = 0; i < 4; i++) {
 
-            {
+            Button b = new Button(this);
+            b.setBackgroundColor(getResources().getColor(R.color.button_default));
+            b.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            b.setId(i);
+            b.setOnTouchListener(new ChoiceTouchListener());
+            switch (b.getId()) {
+                case 0:
+                    b.setText("Button0");
+                    FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(250, 250);
+                    b.setLayoutParams(layoutParams);
+                    break;
+                case 1:
+                    b.setText("Button1");
+                    FrameLayout.LayoutParams layoutParams1 = new FrameLayout.LayoutParams(250, 250);
+                    b.setLayoutParams(layoutParams1);
+                    layoutParams1.topMargin = 300;
 
-                final int X = (int) event.getRawX();
-                final int Y = (int) event.getRawY();
-                switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                    case MotionEvent.ACTION_DOWN:
-                        FrameLayout.LayoutParams lParams = (FrameLayout.LayoutParams) view.getLayoutParams();
-                        _xdelta = X - lParams.leftMargin;
-                        _ydelta = Y - lParams.topMargin;
-                        view.setScaleX(view.getScaleX() + 0.5f);
-                        view.setScaleY(view.getScaleY() + 0.5f);
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        view.setBackgroundColor(Color.rgb(28,118,187));
-                        view.setScaleX(1);
-                        view.setScaleY(1);
-                        break;
-                    case MotionEvent.ACTION_POINTER_DOWN:
-                        break;
-                    case MotionEvent.ACTION_POINTER_UP:
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        view.setBackgroundColor(Color.GREEN);
-                        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) view.getLayoutParams();
-                        layoutParams.leftMargin = X - _xdelta;
-                        layoutParams.topMargin = Y - _ydelta;
-                        layoutParams.rightMargin = 0;
-                        layoutParams.bottomMargin = 0;
-                        view.setLayoutParams(layoutParams);
-                        break;
-                }
+                    break;
+                case 2:
+                    b.setText("Button2");
+                    FrameLayout.LayoutParams layoutParams2 = new FrameLayout.LayoutParams(250, 250);
+                    b.setLayoutParams(layoutParams2);
+                    layoutParams2.topMargin = 600;
 
-                rootLayout.invalidate();
-                return true;
+                    break;
+                case 3:
+                    b.setText("Button3");
+                    FrameLayout.LayoutParams layoutParams3 = new FrameLayout.LayoutParams(250, 250);
+                    b.setLayoutParams(layoutParams3);
+                    layoutParams3.topMargin = 900;
+                    break;
+
             }
+
+            rootLayout.addView(b);
         }
     }
 
+            private final class ChoiceTouchListener implements View.OnTouchListener {
+                private int _xdelta;
+                private int _ydelta;
+
+
+                @Override
+                public boolean onTouch(View view, MotionEvent event) {
+                    {
+
+                         int X = (int) event.getRawX();
+                         int Y = (int) event.getRawY();
+                        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                            case MotionEvent.ACTION_DOWN:
+                                FrameLayout.LayoutParams lParams = (FrameLayout.LayoutParams) view.getLayoutParams();
+                                _xdelta = X - lParams.leftMargin;
+                                _ydelta = Y - lParams.topMargin;
+                                view.setScaleX(view.getScaleX() + 0.5f);
+                                view.setScaleY(view.getScaleY() + 0.5f);
+                                break;
+                            case MotionEvent.ACTION_UP:
+                                view.setBackgroundColor(Color.rgb(28, 118, 187));
+                                view.setScaleX(1);
+                                view.setScaleY(1);
+                                break;
+                            case MotionEvent.ACTION_POINTER_DOWN:
+                                break;
+                            case MotionEvent.ACTION_POINTER_UP:
+                                break;
+                            case MotionEvent.ACTION_MOVE:
+                                view.setBackgroundColor(Color.GREEN);
+                                FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) view.getLayoutParams();
+                                layoutParams.leftMargin = X - _xdelta;
+                                layoutParams.topMargin = Y - _ydelta;
+                                layoutParams.rightMargin = 0;
+                                layoutParams.bottomMargin = 0;
+                                view.setLayoutParams(layoutParams);
+                                break;
+                        }
+
+                        rootLayout.invalidate();
+                        return true;
+                    }
+                }
+            }
+
+
+
+
+/*
+        public class preparation extends View {
+            public preparation(Context context) {
+                super(context);
+            }
+
+            //buton init
+            //Buton a listener eklenecek
+            //tsrgetView init edilecek
+            //button ve targetview için frame hesaplanacak
+
+        }
+
+        //touch handle event içinde button frame center ile target frame center match ediyor ise, bunu superview den kaldır
+        //yada görünmez yap
+        */
 
 
 
 }
+
 
 
 
