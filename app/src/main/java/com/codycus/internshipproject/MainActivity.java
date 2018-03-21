fbec907edf65122ef063ac61210f972aa1288e87
seydabilen <seydabilen0@gmail.com>
2018-03-21 14:11:21 +0300

    Okan's magical touches

--- app/src/main/java/com/codycus/internshipproject/MainActivity.java
+++ app/src/main/java/com/codycus/internshipproject/MainActivity.java
@@ -3,30 +3,26 @@ package com.codycus.internshipproject;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewGroup rootLayout;
-
-
+    private ViewGroup rootLayout,targetLayout;
+    private ArrayList<PuzzlePieceView> interactiveViews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rootLayout = (ViewGroup) findViewById(R.id.view_root);
+        targetLayout=(ViewGroup)findViewById(R.id.target_root);
 
-        FrameLayout.LayoutParams inreactiveViewLayoutParams = new FrameLayout.LayoutParams(400, 400);
-        FrameLayout.LayoutParams inreactiveViewLayoutParams2 = new FrameLayout.LayoutParams(400, 400);
-
-        InteractiveView firstInteractiveView = new InteractiveView(this);
-        firstInteractiveView.setText("Button1");
-        rootLayout.addView(firstInteractiveView, inreactiveViewLayoutParams);
-
-       /* InteractiveView secondInteractiveView = new InteractiveView(this);
-        secondInteractiveView.setText("Button2");
-        rootLayout.addView(secondInteractiveView, inreactiveViewLayoutParams2);*/
+        interactiveViews = new ArrayList<PuzzlePieceView>();
+        for (int i=0; i < 3 ; i++) {
+            PuzzlePieceView puzzlePieceView = new PuzzlePieceView(this, rootLayout, targetLayout, interactiveViews);
+            puzzlePieceView.setText("Piece " + i);
+            interactiveViews.add(puzzlePieceView);
+        }
 
     }
 
--- app/src/main/java/com/codycus/internshipproject/PuzzleEngine.java
+++
@@ -1,8 +0,0 @@
-package com.codycus.internshipproject;
-
-/**
- * Created by Acer on 14.03.2018.
- */
-
-public class PuzzleEngine {
-}
